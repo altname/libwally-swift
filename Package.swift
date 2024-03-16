38dba6119d81a8c4ebef9dc944a2f85e77846cac
Anton <anton.taranovskyi@gmail.com>
2024-03-17 00:21:42 +0100

    swift package manager init

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "LibWally",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11)
+    ],
+    products: [
+        .library(name: "LibWally", targets: ["LibWally"])
+    ],
+    targets: [
+        .target(
+            name: "LibWally",
+            dependencies: []),
+    ]
+)
